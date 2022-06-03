4a9fe251db1f51f2e11545d1d59002a0f1738490
HKwon03 <viplayer03@gmail.com>
2022-06-03 18:18:17 +0900

    commit - 20220603 - kosmo - spring

---
+++ BoardWeb/src/main/java/polymorphism_2_3_1/LgTV.java
@@ -0,0 +1,19 @@
+package polymorphism_2_3_1;
+
+public class LgTV {
+	
+	public void turnOn() {
+		System.out.println("LgTV -- 전원을 켠다");
+	}
+	public void turnoff() {
+		System.out.println("LgTV -- 전원을 끈다");
+	}
+	public void soundUp() {
+		System.out.println("LgTV -- 소리를 높인다");
+	}
+	public void soundDown() {
+		System.out.println("LgTV -- 소리를 낮춘다");
+	}
+	
+
+}
