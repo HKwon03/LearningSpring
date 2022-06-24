eceddec8a135b304d6e0ae27b99773224b34365d
HKwon03 <viplayer03@gmail.com>
2022-06-24 18:32:34 +0900

    commit - 20220624 - kosmo - Mybatis

--- BoardWeb_Mybatis/src/main/java/com/springbook/biz/board/impl/BoardDAOMyBatis.java
+++ BoardWeb_Mybatis/src/main/java/com/springbook/biz/board/impl/BoardDAOMyBatis.java
@@ -8,35 +8,36 @@ import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.util.SqlSessionFactoryBean;
 
 public class BoardDAOMyBatis {
-
+	
 	private SqlSession mybatis;
 	
-	public BoardDAOMyBatis() {	//기본 생성자
+	public BoardDAOMyBatis() {
 		mybatis = SqlSessionFactoryBean.getSqlSessionInstance();
 	}
 	
 	public void insertBoard(BoardVO vo) {
-		System.out.println("MyBatis를 사용해서 insertBoard() 메소드 호출");
-		System.out.println(vo.getTitle());
-		System.out.println(vo.getContent());
-		System.out.println(vo.getWriter());
 		
-		mybatis.insert("BoardDAO.insertBoard", vo);
+		mybatis.insert("BoardDAO.insertBoard",vo);
 		mybatis.commit();
 	}
+	
 	public void updateBoard(BoardVO vo) {
-		mybatis.update("BoardDAO.updateBoard", vo);
+		mybatis.update("BoardDAO.updateBoard",vo);
 		mybatis.commit();
 	}
 	public void deleteBoard(BoardVO vo) {
-		mybatis.delete("BoardDAO.deleteBoard", vo);
+		mybatis.delete("BoardDAO.deleteBoard",vo);
 		mybatis.commit();
 	}
 	public BoardVO getBoard(BoardVO vo) {
-		return (BoardVO) mybatis.selectOne("BoardDAO.getBoard", vo);
+		return (BoardVO) mybatis.selectOne("BoardDAO.getBoard",vo);
 	}
-	public List<BoardVO> getBoardList(BoardVO vo) {
+	public List<BoardVO> getBoardList(BoardVO vo){
+		
+		System.out.println("MyBatis를 사용해서 getBoardLIst() 메소드 호출");
+
 		return mybatis.selectList("BoardDAO.getBoardList", vo);
 	}
 	
+	// getBoard getBoardLIst는 select type이라서 return이 필요하다.
 }
--- BoardWeb_Mybatis/src/main/java/com/springbook/biz/board/impl/BoardServiceClient_MyBatis.java
+++ BoardWeb_Mybatis/src/main/java/com/springbook/biz/board/impl/BoardServiceClient_MyBatis.java
@@ -7,27 +7,27 @@ import com.springbook.biz.board.BoardVO;
 
 public class BoardServiceClient_MyBatis {
 	
-	public static void main(String[] args) throws SQLException {
-	
-	BoardDAOMyBatis boardDAO = new BoardDAOMyBatis();
-	
-	BoardVO vo = new BoardVO();
-	
-	vo.setTitle("myBatis 제목");
-	vo.setWriter("홍길동");
-	vo.setContent("myBatis 내용입니다.");
-
-	boardDAO.insertBoard(vo);
-	
-	//NULL값이 넘어가지 않도록 기본세팅
-	vo.setSearchCondition("TITLE");
-	vo.setSearchKeyword("");
-	List<BoardVO> boardList = boardDAO.getBoardList(vo);
-	for(BoardVO board : boardList) {
-		System.out.println("--->" + board.toString());
+	public static void main(String[] args) throws SQLException{
+		BoardDAOMyBatis boardDAO = new BoardDAOMyBatis();
+		
+		BoardVO vo = new BoardVO();
+		
+		vo.setTitle("new - myBatis 제목");
+		vo.setWriter("홍길동");
+		vo.setContent("myBatis 내용입니다. ");
+		
+		boardDAO.insertBoard(vo);
+		
+		// NULL값이 넘어가지 않도록 기본셋팅
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+		for(BoardVO board : boardList) {
+			System.out.println("---->" + board.toString());
+		}
 	}
 	
 	
 	
-	}
+	
 }
